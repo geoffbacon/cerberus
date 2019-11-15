18025441d786b9a15e99525521f3c8e05fb6b189
Geoff Bacon <bacon@berkeley.edu>
2019-11-15 14:30:39 -0800

    classification dataset creation

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,9 +4,10 @@
 ## Downloads and unpacks the Universal Dependencies corpora.
 ##
 ## At the time of writing, the latest version of the Universal Dependencies
-## data is version 2.4, and this version is hard coded in.
-DIR=back/ud
-wget --quiet -O ud.tgz https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-2988/ud-treebanks-v2.4.tgz
+## data is version 2.5, and this version is hard coded in.
+DIR=back/ud/data/raw
+wget --quiet -O ud.tgz https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3105/ud-treebanks-v2.5.tgz
 tar zxf ud.tgz -C $DIR
-mv $DIR/ud-treebanks-v2.4 $DIR
+mv $DIR/ud-treebanks-v2.5/* $DIR
+rmdir $DIR/ud-treebanks-v2.5
 rm ud.tgz
--- scripts/prepare_ud.py
+++ scripts/prepare_ud.py
@@ -2,39 +2,97 @@
 
 import glob
 import os
+import random
 import re
 from xml.etree import ElementTree
 
-from conllu import parse
+import jsonlines
+import pyconll
 
-THRESHOLD = 50_000
+RAW_UD_DIR = "back/ud/data/raw"
+PROCESSED_UD_DIR = "back/ud/data/processed"
 
 
 def languages_under_threshold(threshold):
-    """Return the names of languages with UD corpora totalling under `threshold` tokens."""
-    file_names = glob.glob("back/ud/**/stats.xml", recursive=True)
-    result = {}
-    for file_name in file_names:
-        directory = os.path.split(os.path.split(file_name)[0])[1]
-        language = re.match(r"UD_(\w+)-\w+", directory).groups(1)[0]
-        tree = ElementTree.parse(file_name)
+    """Return the names of languages with UD corpora totalling at most `threshold` tokens."""
+    pattern = os.path.join(RAW_UD_DIR, "**/stats.xml")
+    filenames = glob.glob(pattern, recursive=True)
+    counts = {}
+    for filename in filenames:
+        dirname = os.path.split(os.path.split(filename)[0])[1]
+        language = re.match(r"UD_(\w+)-\w+", dirname).groups(1)[0]
+        tree = ElementTree.parse(filename)
         root = tree.getroot()
         num_tokens = int(root.find("./size/total/tokens").text)
-        if language not in result:
-            result[language] = num_tokens
+        if language not in counts:
+            counts[language] = num_tokens
         else:
-            result[language] += num_tokens
-    return sorted([lg for lg, num in result.items() if num <= threshold])
+            counts[language] += num_tokens
+    return sorted([lg for lg, num in counts.items() if num <= threshold])
 
 
-languages = languages_under_threshold(THRESHOLD)
-print(languages)
+def conllify(language):
+    """Return the CONLL data structures for the corpora in `language`."""
+    filenames = []
+    corpus_directories = [
+        dirname for dirname in os.listdir(RAW_UD_DIR) if language in dirname
+    ]
+    for dirname in corpus_directories:
+        pattern = os.path.join(RAW_UD_DIR, dirname, "*.conllu")
+        filenames.extend(glob.glob(pattern))
+    return [pyconll.load_from_file(filename) for filename in filenames]
 
-lg = languages[0]
-dir_names = [dir_name for dir_name in os.listdir("back/ud") if lg in dir_name]
-file_names = []
-for dir_name in dir_names:
-    pattern = f"back/ud/{dir_name}/*.conllu"
-    file_names.extend(glob.glob(pattern))
-for file_name in file_names:
-    data = parse(file_name)
+
+def parse_for_classification(sentence):
+    """Return a classification example from `sentence`."""
+    for token in sentence:
+        if token.deprel == "nsubj":
+            try:
+                label = next(iter(token.feats["Number"]))
+                text = sentence.text
+                return {"text": text, "label": label}
+            except (StopIteration, KeyError):
+                return {}
+    return {}
+
+
+PARSE_FUNCTIONS = {"classification": parse_for_classification}
+
+
+def write(lines, filename, task):
+    if task == "classification":
+        with jsonlines.open(filename, mode="w") as writer:
+            writer.write_all(lines)
+    else:
+        with open(filename, "w") as file:
+            file.write("\n".join(lines))
+
+
+def create_data(language, task, split=0.2):
+    conlls = conllify(language)
+    parse_fn = PARSE_FUNCTIONS[task]
+    examples = []
+    for conll in conlls:
+        for sentence in conll:
+            example = parse_fn(sentence)
+            if example:
+                examples.append(example)
+    random.shuffle(examples)
+    n = int(len(examples) * split)
+    train, valid = examples[n:], examples[:n]
+    path = os.path.join(PROCESSED_UD_DIR, task, language)
+    os.makedirs(path, exist_ok=True)
+    if task == "classification":
+        ext = "jsonl"
+    else:
+        ext = "txt"
+    train_filename = os.path.join(path, f"train.{ext}")
+    valid_filename = os.path.join(path, f"valid.{ext}")
+    write(train, train_filename, task)
+    write(valid, valid_filename, task)
+
+
+if __name__ == "__main__":
+    languages = languages_under_threshold(50_000)
+    for language in languages:
+        create_data(language, "classification")
